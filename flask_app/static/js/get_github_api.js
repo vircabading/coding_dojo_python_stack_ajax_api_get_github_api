54a09e43c8226fabb36ce46795bc757a7593467c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-12 18:13:25 -0800

    cleaned up get github api js

--- flask_app/static/js/get_github_api.js
+++ flask_app/static/js/get_github_api.js
@@ -19,14 +19,3 @@ getGithubBtn.addEventListener("click", async function() {
             })
         .catch(err => console.log(err) )
 })
-
-
-// async function getCoderData() {
-//     var response = await fetch("https://api.github.com/users/adion81"); // The await keyword lets js know that it needs to wait until it gets a response back to continue.
-//     var coderData = await response.json();                              // We then need to convert the data into JSON format.
-//     return coderData;
-// }
-
-// var result = getCoderData();
-
-// console.log(result);
